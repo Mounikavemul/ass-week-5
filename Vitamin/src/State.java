9e72f72aa6f1fc9d7f67443d16936f03638a551f
Mounikavemul <90094785+Mounikavemul@users.noreply.github.com>
2021-10-08 23:02:39 -0500

    Add files via upload

---
+++ Vitamin/src/State.java
@@ -0,0 +1,6 @@
+
+public interface State {
+	public abstract void buyVitamin();
+	public abstract void consumeVitamin();
+	public abstract void breakVitamin();
+}
---
+++ Vitamin/src/VitaminTest.java
@@ -0,0 +1,21 @@
+public class VitaminTest {
+
+	public static void main(String[] args) {
+		Vitamin s=new Vitamin();
+		System.out.println(s);
+		s.buyVitamin();
+		s.breakVitamin();
+		s.consumeVitamin();
+		s.buyVitamin();
+		System.out.println(s);
+		s.buyVitamin();
+		s.consumeVitamin();
+		s.breakVitamin();	
+		System.out.println(s);
+		s.buyVitamin();
+		s.consumeVitamin();
+		s.breakVitamin();
+
+	}
+
+}
